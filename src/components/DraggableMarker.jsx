42174d70ddbe69286e0b38c0bb709435b77a9238
Bar Goldenstein <bargolden12@gmail.com>
2024-01-11 18:28:51 +0200

    discord cdn post fix

--- src/components/DraggableMarker.jsx
+++ src/components/DraggableMarker.jsx
@@ -3,7 +3,7 @@ import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
 const icon = new L.Icon({
-    iconUrl: 'https://cdn.discordapp.com/attachments/801426473059614730/1131177633904865390/current-loc.svg',
+    iconUrl: '/current-loc.svg',
     iconSize: [25, 25],
     iconAnchor: [12, 12],
     popupAnchor: [0, -12],
